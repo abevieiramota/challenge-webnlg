f12366d96853a7ed3edafd94968ba0cd0667e04a
Abelardo Vieira Mota <abevieiramota@gmail.com>
2018-12-14 16:57:28 -0300

    adds content selection module

---
+++ script/content_selection.py
@@ -0,0 +1,33 @@
+import logging
+from sklearn.base import BaseEstimator
+from itertools import islice
+
+
+class SelectAllContentSelection(BaseEstimator):
+
+    def __init__(self):
+
+        self.logger = logging.getLogger(self.__class__.__name__)
+        
+
+    def select(self, data):
+
+        return data
+
+
+class SelectFirstNContentSelection(BaseEstimator):
+
+    def __init__(self, n=1, sort_function=lambda x: x):
+
+        self.n = n
+        self.logger = logging.getLogger(self.__class__.__name__)
+        self.sort_function = sort_function
+
+    
+    def select(self, data):
+
+        data_sorted = self.sort_function(data)
+
+        return list(islice(data_sorted, 0, self.n))
+
+        
--- script/text_generation.py
+++ script/text_generation.py
@@ -1,18 +1,23 @@
 class PipelineTextGenerator:
 
-    def __init__(self, sentence_generator, sentence_aggregator, discourse_structurer, lexicalizer):
+    def __init__(self, content_selection, sentence_generator, sentence_aggregator, discourse_structurer, lexicalizer):
 
+        self.content_selection = content_selection
         self.sentence_generator = sentence_generator
         self.sentence_aggregator = sentence_aggregator
         self.discourse_structurer = discourse_structurer
         self.lexicalizer = lexicalizer
 
+
     def generate(self, data):
 
         generated_texts = []
 
+        # rename entry/data -> confusion between entry data and the whole dataset
         for entry in data:
-            sorted_data = self.discourse_structurer.sort(entry)
+
+            selected_entry = self.content_selection.select(entry)
+            sorted_data = self.discourse_structurer.sort(selected_entry)
             sentences = [self.sentence_generator.generate(self.lexicalizer.lexicalize(d)) for d in sorted_data]
             text = self.sentence_aggregator.aggregate(sentences)
 
@@ -23,8 +28,9 @@ class PipelineTextGenerator:
 
 class IfAfterNthProcessPipelineTextGenerator:
 
-    def __init__(self, sentence_generator, sentence_aggregator, discourse_structurer, lexicalizer, processor=lambda x: x, nth=-1):
+    def __init__(self, content_selection, sentence_generator, sentence_aggregator, discourse_structurer, lexicalizer, processor=lambda x: x, nth=-1):
 
+        self.content_selection = content_selection
         self.sentence_generator = sentence_generator
         self.sentence_aggregator = sentence_aggregator
         self.processor = processor
@@ -32,13 +38,16 @@ class IfAfterNthProcessPipelineTextGenerator:
         self.discourse_structurer = discourse_structurer
         self.lexicalizer = lexicalizer
 
+
     def generate(self, data):
 
         generated_texts = []
 
         for entry in data:
 
-            sorted_data = self.discourse_structurer.sort(entry)
+            selected_entry = self.content_selection.select(entry)
+
+            sorted_data = self.discourse_structurer.sort(selected_entry)
 
             sentences = []
             for i, d in enumerate(sorted_data):
