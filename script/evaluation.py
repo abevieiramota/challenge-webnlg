2ad791ba0163d93cefe38e283dd4324a2df7ff14
Abelardo Vieira Mota <abevieiramota@gmail.com>
2019-07-03 20:58:58 -0300

    +

--- script/evaluation.py
+++ script/evaluation.py
@@ -9,6 +9,8 @@ test = corpus.subset(datasets=['test'])
 
 MODEL_DIR = '../model'
 
+EVALUATION_SETS = ['all-cat', 'old-cat', 'new-cat', '1size', '2size', '3size', '4size', '5size', '6size', '7size']
+
 if not os.path.isdir(MODEL_DIR):
     os.mkdir(MODEL_DIR)
 
@@ -28,17 +30,17 @@ TER_RE = re.compile(r'Total\ TER:\ ([\d\.]+)\ \(')
 ALL_CAT_TO_TER_FILE_RE = re.compile(r'(.*)(\.txt)')
 
 
-def evaluate_texts(all_cat_filepath):
+def evaluate_texts(preprocessed_filepath, evaluation_set):
 
     result = {}
 
-    with open(all_cat_filepath, 'rb') as f:
+    with open(preprocessed_filepath, 'rb') as f:
         # bleu
         bleu_result = subprocess.run(['../evaluation/webnlg2017/webnlg-baseline-master/multi-bleu.perl',
                                  '-lc',
-                                 '../evaluation/webnlg2017/webnlg-automatic-evaluation/references/gold-all-cat-reference0.lex',
-                                 '../evaluation/webnlg2017/webnlg-automatic-evaluation/references/gold-all-cat-reference1.lex',
-                                 '../evaluation/webnlg2017/webnlg-automatic-evaluation/references/gold-all-cat-reference2.lex'],
+                                 f'../evaluation/webnlg2017/webnlg-automatic-evaluation/references/gold-{evaluation_set}-reference0.lex',
+                                 f'../evaluation/webnlg2017/webnlg-automatic-evaluation/references/gold-{evaluation_set}-reference1.lex',
+                                 f'../evaluation/webnlg2017/webnlg-automatic-evaluation/references/gold-{evaluation_set}-reference2.lex'],
                                  stdout=subprocess.PIPE,
                                  input=f.read())
 
@@ -47,8 +49,8 @@ def evaluate_texts(all_cat_filepath):
     # meteor
     meteor_result = subprocess.run(['java', '-Xmx2G', '-jar',
                                     '../evaluation/webnlg2017/meteor-1.5/meteor-1.5.jar',
-                                    all_cat_filepath,
-                                    '../evaluation/webnlg2017/webnlg-automatic-evaluation/references/gold-all-cat-reference-3ref.meteor',
+                                    preprocessed_filepath,
+                                    f'../evaluation/webnlg2017/webnlg-automatic-evaluation/references/gold-{evaluation_set}-reference-3ref.meteor',
                                     '-l', 'en', '-norm', '-r', '3', '-a',
                                     '../evaluation/webnlg2017/meteor-1.5/data/paraphrase-en.gz'],
                                     stdout=subprocess.PIPE)
@@ -56,11 +58,11 @@ def evaluate_texts(all_cat_filepath):
     result['meteor'] = float(METEOR_RE.findall(meteor_result.stdout.decode('utf-8'))[0])
 
     # ter
-    ter_filepath = ALL_CAT_TO_TER_FILE_RE.sub(r'\1_ter\2', all_cat_filepath)
+    ter_filepath = ALL_CAT_TO_TER_FILE_RE.sub(r'\1_ter\2', preprocessed_filepath)
 
     ter_result = subprocess.run(['java', '-jar', '../evaluation/webnlg2017/tercom-0.7.25/tercom.7.25.jar',
                                  '-r',
-                                 '../evaluation/webnlg2017/webnlg-automatic-evaluation/references/gold-all-cat-reference-3ref.ter',
+                                 f'../evaluation/webnlg2017/webnlg-automatic-evaluation/references/gold-{evaluation_set}-reference-3ref.ter',
                                  '-h',
                                  ter_filepath],
                                  stdout=subprocess.PIPE)
@@ -70,10 +72,11 @@ def evaluate_texts(all_cat_filepath):
     return result
 
 
-def evaluate_model(model, model_name):
+def evaluate_model(model, model_name, evaluation_set='all-cat'):
 
     texts_filepath = f'../model/{model_name}.txt'
-    all_cat_filepath = f'../model/{model_name}_all-cat.txt'
+
+    preprocessed_filepath = f'../model/{model_name}_{evaluation_set}.txt'
 
     # generate the texts
 
@@ -87,4 +90,4 @@ def evaluate_model(model, model_name):
 
     preprocess_to_evaluate(texts_filepath, model_name)
 
-    return evaluate_texts(all_cat_filepath)
+    return evaluate_texts(preprocessed_filepath, evaluation_set)
